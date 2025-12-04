55d8e49a2bebbfa7ae6061a5c68783a5bc13f41a
jsnaranjo6 <95392079+jsnaranjo6@users.noreply.github.com>
2025-12-04 01:24:39 -0500

    Add files via upload

---
+++ public/src/src:js:constants.js
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 export const MAX_NUMBER = 1000000; // 7 slots\
+export const MIN_NUMBER = 1;\
+export const SPIN_TIME = 1400; // ms base\
+export const COUNTDOWN_SECONDS = 45;\
+\
+export const LS_KEY = 'purchasedNumbers_v1';\
+export const LS_USERS = 'users_v1';\
+export const LS_META = 'purchasedMeta_v1';\
+\
+export const MS_28D = 28*24*60*60*1000;\
+}
